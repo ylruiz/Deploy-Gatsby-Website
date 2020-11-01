b54cb3ba180aae21319260cb8bf691768d18d0a6
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2020-11-01 12:21:54 +0100

    Version 1.0.3

--- component---src-pages-404-js-e502ce3aca8babe0c3b6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),o=n.n(t),r=n("7oih");a.default=function(){return o.a.createElement(r.a,{title:"404"},o.a.createElement("main",{id:"page_404"},o.a.createElement("h1",null,"404: Page Not Found"),o.a.createElement("h2",null,"Oops! The page you are looking for has been removed or relocated")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e502ce3aca8babe0c3b6.js.map
